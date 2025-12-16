36104f7f527608d996e3e3d07c7ddff910b28da5
Guillermo Álvarez López <guillealvarezlopezz@gmail.com>
2025-12-16 17:11:37 +0100

    UI and DinoBaseATT

---
+++ Source/AttributeSystem/Private/MMC_CritChance.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "MMC_CritChance.generated.h"
+
+UCLASS()
+class ATTRIBUTESYSTEM_API UMMC_CritChance : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UMMC_CritChance();
+
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+
+private:
+	FGameplayEffectAttributeCaptureDefinition StrengthCapture;
+	FGameplayEffectAttributeCaptureDefinition InstinctCapture;
+};
---
+++ Source/AttributeSystem/Public/MMC_CritChance.cpp
@@ -0,0 +1,49 @@
+﻿#include "MMC_CritChance.h"
+
+#include "ACFPrimaryAttributeSet.h"
+#include "PangeaAttributeSet.h"
+#include "GameplayEffectTypes.h"
+
+
+UMMC_CritChance::UMMC_CritChance()
+{
+	const FStructProperty* StrengthStruct =
+	FindFProperty<FStructProperty>(UACFPrimaryAttributeSet::StaticClass(),
+	GET_MEMBER_NAME_CHECKED(UACFPrimaryAttributeSet, Strength));
+
+	const FStructProperty* InstinctStruct =
+		FindFProperty<FStructProperty>(UPangeaAttributeSet::StaticClass(),
+		GET_MEMBER_NAME_CHECKED(UPangeaAttributeSet, Instinct));
+
+	
+	FGameplayAttribute SAttr(const_cast<FStructProperty*>(StrengthStruct));
+	FGameplayAttribute IAttr(const_cast<FStructProperty*>(InstinctStruct));
+
+	
+	StrengthCapture = FGameplayEffectAttributeCaptureDefinition(
+		SAttr,
+		EGameplayEffectAttributeCaptureSource::Source,
+		false);
+
+	InstinctCapture = FGameplayEffectAttributeCaptureDefinition(
+		IAttr,
+		EGameplayEffectAttributeCaptureSource::Source,
+		false);
+
+	RelevantAttributesToCapture.Add(StrengthCapture);
+	RelevantAttributesToCapture.Add(InstinctCapture);
+}
+
+float UMMC_CritChance::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	float Strength = 0.f;
+	float Instinct = 0.f;
+
+	FAggregatorEvaluateParameters Params;
+
+	GetCapturedAttributeMagnitude(StrengthCapture, Spec,  Params, Strength);
+	GetCapturedAttributeMagnitude(InstinctCapture, Spec, Params, Instinct);
+	
+	return (Strength * 0.5f) + (Instinct * 0.5f);
+}
+
